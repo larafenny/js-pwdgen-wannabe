8851c9cf964c4297ec2f65e48ee028847d87dfe8
Lara Filippone <lara.filippone@gmail.com>
2022-03-04 08:40:31 +0100

    generator psw as adding of name+surname+color

--- js/script.js
+++ js/script.js
@@ -5,7 +5,10 @@ let name = prompt("What's your name?");
 let name = prompt("What's your surname?");
 
 // Richiesta colore preferito all'utente
-let color = prompt("What's your favourite colour?");
+let color = prompt("What's your favourite color?");
 
-// Genero  numero a caso
+// Genero numero a caso
 let randomNumber = Math.floor(Math.random() * 100);
+
+// Genero psw come somma di name+surname+color+randomNumber
+let psw = name + surname + color + randomNumber;
