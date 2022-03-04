2cdb086abbc6fb06f70ee8b91ea3bb662bfaf2de
Lara Filippone <lara.filippone@gmail.com>
2022-03-04 09:30:58 +0100

    request color

--- js/script.js
+++ js/script.js
@@ -3,3 +3,6 @@ let nameUser = prompt("What's your name?");
 
 // Richiesta cognome all'utente
 let surname = prompt("What's your surname?");
+
+// Richiesta colore preferito all'utente
+let color = prompt("What's your favourite color?");
