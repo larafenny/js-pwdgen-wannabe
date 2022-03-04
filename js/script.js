d373279b5b246e22a12536497290145a578ce923
Lara Filippone <lara.filippone@gmail.com>
2022-03-04 09:29:52 +0100

    request name

--- js/script.js
+++ js/script.js
@@ -1,14 +1,2 @@
 // Richiesta nome all'utente
-let name = prompt("What's your name?");
-
-// Richiesta cognome all'utente
-let name = prompt("What's your surname?");
-
-// Richiesta colore preferito all'utente
-let color = prompt("What's your favourite color?");
-
-// Genero numero a caso
-let randomNumber = Math.floor(Math.random() * 100);
-
-// Genero psw come somma di name+surname+color+randomNumber
-let psw = name + surname + color + randomNumber;
+let nameUser = prompt("What's your name?");
