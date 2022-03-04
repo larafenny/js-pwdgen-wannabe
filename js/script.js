bc376f16c2bcff75ba5e3780e692891014939690
Lara Filippone <lara.filippone@gmail.com>
2022-03-04 08:38:43 +0100

    random number

--- js/script.js
+++ js/script.js
@@ -6,3 +6,6 @@ let name = prompt("What's your surname?");
 
 // Richiesta colore preferito all'utente
 let color = prompt("What's your favourite colour?");
+
+// Genero  numero a caso
+let randomNumber = Math.floor(Math.random() * 100);
